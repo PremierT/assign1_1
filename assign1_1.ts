6172723cc0d14d18ca103110b87a096858cedb05
PremierT <peekunggamec@gmail.com>
2026-06-18 14:06:17 +0700

    Assignment1

---
+++ assign1_1.ts
@@ -0,0 +1,20 @@
+let productName: string = "T-shirt";
+let pricePerUnit: number = 200;
+let quantity: number = 6;
+let isMember: boolean = false;
+
+let totalPrice: number = pricePerUnit * quantity;
+let discount: boolean = totalPrice > 1000 || isMember;
+let discountrate: number;
+if(discount == true){
+	discountrate = 0.10;
+}else{
+	discountrate = 0;
+}
+
+
+console.log("Product is %s, Quantity is %d.\n",productName,quantity);
+console.log("Totalprice is %d.\n",totalPrice);
+console.log("Discount?: %s.\n",discount);
+let netprice: number = totalPrice-(totalPrice*discountrate);
+console.log("Net price is %d.",netprice);
